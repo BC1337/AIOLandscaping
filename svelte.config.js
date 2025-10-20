a7b9115c36e1520ae8d37a5f144eecced7c3d224
BC1337 <brettcomrie@gmail.com>
2025-10-20 14:19:56 -0500

    trying a different config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,7 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import vercel from '@sveltejs/adapter-vercel';
-import { preprocess } from 'svelte/compiler'; // optional, if you need preprocessing
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Use Vite preprocessing (handles SCSS, PostCSS, etc.)
-	preprocess: [],
-
-	kit: {
-		adapter: vercel()
-	}
+export default {
+  kit: {
+    adapter: vercel()
+  }
 };
-
-export default config;
